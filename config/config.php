2b9185400a68b66d622bf7b4b70c7c885428eeaf
FuchsiaIO <siras@live.com>
2015-11-20 15:14:26 -0500

    Set up db configs with yml -- prep for migrations

--- config/config.php
+++ config/config.php
@@ -3,8 +3,7 @@
 require_once 'constants.php';
 require_once FUCHSIA_ROOT_PATH.'/fuchsia/framework.php';
 require_once 'autoload.php';
-if(USE_ACTIVE_RECORD)
-  require_once 'database.php';
+require_once 'database.php';
 
 require_once 'routes.php';
 require_once 'template_registry.php';
